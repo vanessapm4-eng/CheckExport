1776a18877ba1eb6940eec1634cb8c84319667a2
CamilaGiraldoGiraldo <133153684+CamilaGiraldoGiraldo@users.noreply.github.com>
2026-06-09 21:47:09 -0500

    Ultimas, se agrega funcion de eliminar y editar predio

--- apps/predios/urls.py
+++ apps/predios/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.landing, name='landing'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('predios/nuevo/', views.crear_predio, name='crear_predio'),
+    path('predios/<int:predio_pk>/editar/', views.editar_predio, name='editar_predio'),
+    path('predios/<int:predio_pk>/eliminar/', views.eliminar_predio, name='eliminar_predio'),
 ]
--- apps/predios/views.py
+++ apps/predios/views.py
@@ -34,3 +34,28 @@ def crear_predio(request):
         messages.success(request, f'Predio "{predio.nombre}" creado exitosamente.')
         return redirect('dashboard')
     return render(request, 'predios/form.html')
+
+@login_required
+def editar_predio(request, predio_pk):
+    predio = get_object_or_404(Predio, pk=predio_pk, usuarios=request.user)
+    if request.method == 'POST':
+        predio.nombre               = request.POST.get('nombre')
+        predio.registro_ica         = request.POST.get('registro_ica')
+        predio.municipio            = request.POST.get('municipio')
+        predio.departamento         = request.POST.get('departamento')
+        predio.vereda               = request.POST.get('vereda', '')
+        predio.estado_habilitacion  = request.POST.get('estado_habilitacion')
+        predio.save()
+        messages.success(request, f'Predio "{predio.nombre}" actualizado correctamente.')
+        return redirect('dashboard')
+    return render(request, 'predios/form_editar.html', {'predio': predio})
+
+@login_required
+def eliminar_predio(request, predio_pk):
+    predio = get_object_or_404(Predio, pk=predio_pk, usuarios=request.user)
+    if request.method == 'POST':
+        nombre = predio.nombre
+        predio.delete()
+        messages.success(request, f'Predio "{nombre}" eliminado.')
+        return redirect('dashboard')
+    return render(request, 'predios/confirmar_eliminar.html', {'predio': predio})
